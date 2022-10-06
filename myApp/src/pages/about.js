e40a9e586e08c5ba0b6f6836a027d5ec41228c8b
Martin <info@christ-kind.de>
2022-10-06 23:03:38 +0200

    adding About page to site

---
+++ myApp/src/pages/about.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link, graphql } from 'gatsby';
+
+export default ({ data }) => (
+  <React.Fragment>
+    <h1> Test </h1>
+    <h2>das ist der title {data.site.siteMetadata.title}</h2>
+    <div>{data.site.siteMetadata.description}</div>
+    <Link to="/">Back to home</Link>
+  </React.Fragment>
+)
+
+export const query = graphql `
+  query {
+    site {
+      siteMetadata {
+        author,
+        description,
+        title
+      }
+    }
+  }
+`
